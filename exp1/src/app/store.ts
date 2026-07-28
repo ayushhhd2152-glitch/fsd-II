7684cfc8ab1d2976a5f532d6f78f6dd630799a97
ayushhhd2152-glitch <ayushhhd2152@gmail.com>
2026-07-28 15:05:50 +0530

    Add files via upload

---
+++ exp1/src/app/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+import postsReducer from '../features/posts/postsSlice';
+import platformReducer from '../features/platform/platformSlice';
+
+export const store = configureStore({
+  reducer: {
+    posts: postsReducer,
+    platform: platformReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
